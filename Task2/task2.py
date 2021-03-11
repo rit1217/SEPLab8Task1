41030cbf638166d67587c98357d6b781a8cd49d6
rit1217 <57401346+rit1217@users.noreply.github.com>
2021-03-11 18:26:53 +0700

    ui

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,1 +1,35 @@
-#something
+import sys
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtPrintSupport import *
+
+
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.points = []
+
+        self.resize(400, 300)
+
+        self.label = QLabel(self)
+        self.label.setText("Drag mouse to draw")
+        self.label.move(140, 260)
+
+        self.button = QPushButton(self)
+        self.button.setText("Clear")
+        self.button.move(140, 280)
+        self.button.clicked.connect(self.clear_points)
+
+        
+def main():
+    app = QApplication(sys.argv)
+
+    w = Simple_drawing_window()
+    w.show()
+
+    return app.exec_()
+
+if __name__ == "__main__":
+    sys.exit(main())

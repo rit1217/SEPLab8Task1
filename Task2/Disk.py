ee2bcf1713a89d815752ffd1e6bb2d1ba7a825e9
Patthiya193 <79250844+Patthiya193@users.noreply.github.com>
2021-03-11 18:59:51 +0700

    newpos, clear func

--- Task2/Disk.py
+++ Task2/Disk.py
@@ -1,3 +1,4 @@
+from turtle import *
 class Disk():
     def __init__(self, x, y, h, w, c):
         self.x = x
@@ -5,4 +6,28 @@ class Disk():
         self.h = h
         self.w = w
         self.c = c
-    
+
+
+    def newpos(self, x, y):
+        self.cleardisk()
+        self.x = x
+        self.y = y
+        self.showdisk()
+
+    def cleardisk(self):
+        penup()
+        goto(self.x, self.y)
+        pendown()
+        pencolor("white")
+        fillcolor("white")
+        begin_fill()
+        forward(self.w / 2)
+        left(90)
+        forward(self.h)
+        left(90)
+        forward(self.w)
+        left(90)
+        forward(self.h)
+        left(90)
+        forward(self.w / 2)
+        end_fill()

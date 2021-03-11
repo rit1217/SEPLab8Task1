e2f666470a614b4ce3ab88ebef0f17364298eda9
rit1217 <57401346+rit1217@users.noreply.github.com>
2021-03-11 17:56:58 +0700

    edit file2

--- simple_drawing_window2.py
+++ simple_drawing_window2.py
@@ -3,11 +3,11 @@ import sys
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
-import simple_drawing_window
+from simple_drawing_window import *
 
 class simple_drawing_window2( simple_drawing_window ):
     def __init__( self ):
-        super.__init__()
+        super().__init__()
 
     def paintEvent( self, e ):
         p = QPainter()

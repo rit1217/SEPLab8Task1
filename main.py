c5ec858f9c1fb29740d79e40befa7a7b99f6d57d
rit1217 <57401346+rit1217@users.noreply.github.com>
2021-03-11 18:11:03 +0700

    emm

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from simple_drawing_window2 import *
 from simple_drawing_window1 import *
+
 def main():
-	app = QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
     w1 = simple_drawing_window1()
-	w2 = simple_drawing_window2()
+    w2 = simple_drawing_window2()
     
-	w1.show()
+    w1.show()
     w2.show()
-    
-	return app.exec_()
-	#rabbit
+
+    return app.exec_()
+    #rabbit
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
